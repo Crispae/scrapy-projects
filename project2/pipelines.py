61b1d76c13bebd52ceb07eaf94b11e731752e964
Saurav kumar <Sritwik396@gmail.com>
2020-11-16 20:36:08 +0530

    A databse pipline is being added to scrapy

--- project2/pipelines.py
+++ project2/pipelines.py
@@ -6,8 +6,51 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
+import os
+
 
 
 class Project2Pipeline:
+
+
+    def create_db(self):
+        conn = sqlite3.connect("quotes.db")
+        curr = conn.cursor()
+
+        curr.execute(""" CREATE TABLE quotes(
+
+            Author text,
+            Quotes text,
+            Tags   text
+        )""")
+
+        conn.commit()
+        conn.close()
+        
     def process_item(self, item, spider):
-        return item
+
+        if len(item.keys()) == 3:
+
+
+    
+        
+            data = [item["Author"],item["quotes"],item["Tags"],]
+
+        
+            if os.path.isfile("quotes.db"):
+                conn = sqlite3.connect("quotes.db")
+                curr = conn.cursor()
+
+                curr.execute(""" INSERT INTO quotes VALUES(?,?,?)""",data)
+                conn.commit()
+            else:
+                self.create_db()
+                conn = sqlite3.connect("quotes.db")
+                curr = conn.cursor()
+
+                curr.executemany(""" INSERT INTO quotes VALUES(?,?,?)""",data)
+                conn.commit()
+
+
+            return item
